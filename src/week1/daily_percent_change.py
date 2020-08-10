db61a35c84a1902d36c3db17e91a1391a87e74d8
jy.oh <jy.oh@widerplanet.com>
2020-08-10 21:20:02 +0900

    add dpc

---
+++ src/week1/daily_percent_change.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from scipy import stats
+import yfinance
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+
+
+def get_data(stock_name, start, end):
+    return pdr.get_data_yahoo(stock_name, start=start, end=end)
+
+
+if __name__ == '__main__':
+    start_date = '2018-05-04'
+    end_date = '2019-09-06'
+    id1 = '005930.KS'
+    id2 = 'MSFT'
+    df1 = get_data(id1, start_date, end_date)
+    df2 = get_data(id2, start_date, end_date)
+    df1_dpc = (df1.Close - df1.Close.shift(1)) / df1.Close.shift(1) * 100
+    df2_dpc = (df2.Close - df2.Close.shift(1)) / df2.Close.shift(1) * 100
+    # df2_dpc = ((df2.Close - df2.Close.shift(1)) / df2.Close.shift(1) - 1) * 100
+
+    df1_dpc.iloc[0] = 0
+    df2_dpc.iloc[0] = 0
+
+    # plt.hist(df1_dpc, bins=18)
+    # plt.show()
+
+    df1_dpc1 = df1.Close.pct_change()
+    # 비슷도 안함
+    print(f"pct_change : {df1_dpc1.head()}")
+    print(f"calced : {df1_dpc.head()}")
+
+    # 누적합
+    cs1 = df1_dpc.cumsum()
+    cs2 = df2_dpc.cumsum()
+
+    # plt.subplot(211)
+    plt.plot(df1.index, cs1, 'b', label=f"{id1}")
+    plt.plot(df2.index, cs2, 'r--', label=f"{id2}")
+    plt.ylabel('Change % cumsum')
+    plt.grid(True)
+    plt.legend(loc='best')
+
+    # plt.subplot(212)
+    # plt.plot(df1.index, df1_dpc, 'b', label=f"{id1}")
+    # plt.plot(df2.index, df2_dpc, 'r--', label=f"{id2}")
+    # plt.ylabel('Change %')
+    # plt.grid(True)
+    # plt.legend(loc='best')
+
+    plt.show()
+
--- src/week1/max_dropdown.py
+++ src/week1/max_dropdown.py
@@ -18,14 +18,16 @@ if __name__ == '__main__':
     df2_id = '^DJI'
     df1 = get_data(df1_id, start_date, end_date)
     df2 = get_data(df2_id, start_date, end_date)
-
-
     window = 252  # 기간으로 1년 중 개장일이 252 일이라는 가정
+    # min_period 는 데이터의 갯수가 window 에 못미치더라도 만족하면 작업을 수행할 최소수
     peak = df1['Adj Close'].rolling(window, min_periods=1).max()  # 최고가는 MAX
     dd = df1['Adj Close']/peak - 1.0  # dropdown 은 최고치 대비 현재 종가
     max_dd = dd.rolling(window, min_periods=1).min()  # 종가 중 최저치
 
+    # figsize 차트의 가로인치, 세로인치
     plt.figure(figsize=(9, 7))
+
+    # 화면에 여러플롯을 나눠서 그리는 기능
     plt.subplot(211)  # 2행 1열 1행?
     df1['Close'].plot(label=df1_id, title=f"{df1_id} MDD", grid=True, legend=True)
     plt.subplot(212)
